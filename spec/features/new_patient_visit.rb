50dc37d7b1c37af79f3a96e1c3af89e2de3f2b7a
Deepak N <endeep123@gmail.com>
2014-06-25 10:55:07 +0530

    Refactoring to reduce clutterness in spec

--- spec/features/new_patient_visit.rb
+++ spec/features/new_patient_visit.rb
@@ -6,19 +6,18 @@ feature "new patient visit" do
     end
 
     given(:new_patient) do
-        {:given_name => "Ram#{Time.now.to_i}", :family_name => 'Singh',
-         :gender => 'Male', :age => {:years => 40}, :village => 'Ganiyari'}
+        {:given_name => "Ram#{Time.now.to_i}", :family_name => 'Singh', :gender => 'Male', :age => {:years => 40}, :village => 'Ganiyari'}
     end
 
-    scenario "clinicians consulting the patient" do
+    scenario "registration and consultation" do
     	go_to_app(:registration) do
 			register_new_patient(:patient => new_patient, :visit_type => 'OPD')
             visit_page.should_be_current_page
     	end
 
     	go_to_app(:clinical) do
-			patient_search_page.should_have_patient(new_patient[:given_name])
-			patient_search_page.show_active_patient(new_patient[:given_name])
+			patient_search_page.should_have_patient(new_patient)
+			patient_search_page.show_active_patient(new_patient)
     	end
     end
 end
--- spec/pages/clinical/patient_search_page.rb
+++ spec/pages/clinical/patient_search_page.rb
@@ -1,12 +1,13 @@
 class Clinical::PatientSearchPage < Page
-	def show_active_patient(text)
-		fill_in "patientIdentifierInList", :with => text
-		active_patient(text).click
+	def show_active_patient(patient)
+		search_text = patient[:given_name]
+		fill_in "patientIdentifierInList", :with => search_text
+		active_patient(search_text).click
 		wait_for_overlay_to_be_hidden
 	end
 
-	def should_have_patient(text)
-		active_patient(text)
+	def should_have_patient(patient)
+		active_patient(patient[:given_name])
 	end
 
 	private
