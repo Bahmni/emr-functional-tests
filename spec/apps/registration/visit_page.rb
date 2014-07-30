c8ca20f1e37191afe38056ac22d6725c523948d4
Vinkesh Banka <vinkesh.banka@gmail.com>
2014-07-30 11:31:37 +0530

    Banka | Fixing test

--- spec/apps/registration/visit_page.rb
+++ spec/apps/registration/visit_page.rb
@@ -3,8 +3,8 @@ class Registration::VisitPage < Page
 
     def save_new_patient_visit(visit_info)
         fill_in("Fee", :with => visit_info[:fee])
-        fill_in("Weight", :with => visit_info[:weight])
-        fill_in("Height", :with => visit_info[:height])
+        fill_in("WEIGHT", :with => visit_info[:weight])
+        fill_in("HEIGHT", :with => visit_info[:height])
         fill_in("COMMENTS", :with => visit_info[:comments])
         click_on("Save and Print")
         wait_for_overlay_to_be_hidden
