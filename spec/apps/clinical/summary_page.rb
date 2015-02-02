80c9fccc2614d0eb774e878e3d9a9f2b0d2b972c
Sushma V <sushmav@thoughtworks.com>
2015-02-02 11:49:00 +0530

    Sushma|Fixing test failures

--- spec/apps/clinical/summary_page.rb
+++ spec/apps/clinical/summary_page.rb
@@ -1,10 +1,10 @@
-class Clinical::SummaryPag < Page
+class Clinical::SummaryPage < Page
 
-  @@treatment_section = "#treatment-summary"
+  TREATMENT_SECTION = "#treatment-summary"
 
   def verify_existing_drugs(sections)
     sections.each do |section|
-      table = page.find(@@treatment_section, text: section['visit_date'])
+      table = page.find(TREATMENT_SECTION, text: section['visit_date'])
       section['drugs'].each do |drug|
         expect(table).to have_content(drug)
       end
@@ -13,7 +13,7 @@ class Clinical::SummaryPag < Page
   end
 
   def verify_new_drugs(*drugs)
-    verify_drug_details(@@treatment_section, *drugs)
+    verify_drug_details(TREATMENT_SECTION, *drugs)
   end
 
 end
