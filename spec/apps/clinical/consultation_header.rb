436be5805626d42678cc6e9ca032ada99c2cd4e2
chethanTw <chethandec22@gmail.com>
2015-02-09 18:58:48 +0530

    1613 | Chethan | Fixing functional test

--- spec/apps/clinical/consultation_header.rb
+++ spec/apps/clinical/consultation_header.rb
@@ -3,15 +3,9 @@ module Clinical::ConsultationHeader
         go_to_tab "Observations"
     end
 
-    def go_to_visit_page
-        # go_to_tab "Visit"
-        debugger
+    def go_to_dashboard_page
       find('#dashboard-link').click
       wait_for_overlay_to_be_hidden
-        find(".visits i[title='Current Visit']").click
-        wait_for_overlay_to_be_hidden
-
-        # patient_dashboard_page.navigate_to_current_visit
     end
 
     def save
