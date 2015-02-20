dabd8cf5eb1b91c76fa9e4a181f27e943d5dd8e8
sravanthi17 <sravanns@thoughtworks.com>
2015-02-20 16:10:45 +0530

    Sravanthi, Sushma | Verifying undo disposition

--- spec/apps/adt/patient_search_page.rb
+++ spec/apps/adt/patient_search_page.rb
@@ -4,16 +4,24 @@ class Adt::PatientSearchPage < Page
       search_patient_in_all_tab( patient)
     end
 
-    def view_patient_from_To_admit_tab(patient)
+    def view_patient_from_to_admit_tab(patient)
       search_patient_in_tab("To Admit", patient)
       end
 
-    def view_patient_from_To_discharge_tab(patient)
+    def view_patient_from_to_discharge_tab(patient)
       search_patient_in_tab("To Discharge", patient)
       end
 
     def view_patient_from_admitted_tab(patient)
       search_patient_in_tab("Admitted", patient)
+    end
+
+    def verify_patient_not_found_in_to_admit_tab(patient)
+       go_to_tab("To Admit")
+       fill_in "patientIdentifier", :with => patient
+       if(!assert_no_selector(".active-patient"))
+         expect(find('.active-patient')).to have_no_content(patient)
+       end
     end
 
     private
