3813359a40286df814f9f5a7bf0f1e193bcbaac7
sravanthi17 <sravanns@thoughtworks.com>
2015-02-20 11:55:45 +0530

    Sravanthi, Sushma | #1704 | Added test to discharge the admitted patient

---
+++ spec/apps/adt/patient_dashboard_page.rb
@@ -0,0 +1,31 @@
+class Adt::PatientDashboardPage < Page
+
+  def perform_action(action_details)
+    select(action_details[:action], :from => "dispositionAction")
+    find("#adtNotes textArea").set(action_details[:notes]) if action_details.has_key? :notes
+  end
+
+  def perform_admit_action(action_details)
+    perform_action(action_details)
+    click_on "Admit"
+    wait_for_overlay_to_be_hidden
+  end
+
+  def perform_cancel_action(action_details)
+    perform_action(action_details)
+    click_on "Cancel"
+    wait_for_overlay_to_be_hidden
+  end
+
+  def perform_discharge_action(action_details)
+    perform_action(action_details)
+    click_on "Discharge"
+    wait_for_overlay_to_be_hidden
+  end
+
+  def perform_undo_discharge_action(action_details)
+    perform_action(action_details)
+    click_on "Undo Discharge"
+    wait_for_overlay_to_be_hidden
+  end
+end
--- spec/features/adt_flow.rb
+++ spec/features/adt_flow.rb
@@ -4,24 +4,53 @@ feature "existing patient admit and discharge verification" do
   scenario "verify admit and discharge" do
 
     patient1 = "Test AdtFlow"
-    disposition_details = {:disposition => "Admit Patient", :notes => "Admit the patient"}
-
+    admit_disposition_details = {:disposition => "Admit Patient", :notes => "Admit the patient"}
+    discharge_disposition_details = {:disposition => "Discharge Patient", :notes => "Discharge the patient"}
+    admit_details = {:action => "Admit Patient", :notes => "Admit the patient"}
+    discharge_details = {:action => "Discharge Patient", :notes => "Discharge the patient"}
+    undo_discharge_details = {:action => "Undo Discharge", :notes => "Undo Discharge"}
 
     log_in_to_app(:clinical, :location => 'OPD-1') do
       patient_search_page.view_patient_from_active_tab(patient1)
       patient_dashboard_page.start_consultation
       disposition_page.go_to_tab("Disposition")
-      disposition_page.provide_disposition(disposition_details)
+      disposition_page.provide_disposition(admit_disposition_details)
       disposition_page.save
 
       disposition_page.go_to_dashboard_page
-      patient_dashboard_page.verify_disposition_details(disposition_details)
+      patient_dashboard_page.verify_disposition_details(admit_disposition_details)
       patient_dashboard_page.navigate_to_current_visit
-      visit_page.verify_disposition_details(disposition_details)
+      visit_page.verify_disposition_details(admit_disposition_details)
       visit_page.navigate_to_home
       go_to_app("adt") do
-       patient_search_page.view_patient_from_To_Admit_tab(patient1)
+        patient_search_page.view_patient_from_To_admit_tab(patient1)
+        patient_dashboard_page.verify_disposition_details(admit_disposition_details)
+        patient_dashboard_page.perform_admit_action(admit_details)
+        patient_search_page.view_patient_from_admitted_tab(patient1)
       end
+    end
+
+    log_in_to_app(:clinical, :location => 'OPD-1') do
+      patient_search_page.view_patient_from_active_tab(patient1)
+      patient_dashboard_page.start_consultation
+      disposition_page.go_to_tab("Disposition")
+      disposition_page.provide_disposition(discharge_disposition_details)
+      disposition_page.save
+
+      disposition_page.go_to_dashboard_page
+      patient_dashboard_page.verify_disposition_details(discharge_disposition_details)
+      patient_dashboard_page.navigate_to_current_visit
+      visit_page.verify_disposition_details(discharge_disposition_details)
+      visit_page.navigate_to_home
+      go_to_app("adt") do
+        patient_search_page.view_patient_from_To_discharge_tab(patient1)
+        patient_dashboard_page.verify_disposition_details(discharge_disposition_details)
+        patient_dashboard_page.perform_discharge_action(discharge_details)
+        patient_search_page.view_patient_from_all_tab(patient1)
+        patient_dashboard_page.perform_undo_discharge_action(undo_discharge_details)
+        patient_search_page.view_patient_from_admitted_tab(patient1)
+      end
+    end
+
   end
-  end
-  end
+end
