da115295fb12c386dd4901b132cea349340ac19e
Vinay Venu <vinayv@thoughtworks.com>
2015-03-10 12:24:04 +0530

    Divya, Vinay | Delete a diagnosis

--- spec/apps/clinical/diagnosis_page.rb
+++ spec/apps/clinical/diagnosis_page.rb
@@ -4,9 +4,38 @@ class Clinical::DiagnosisPage < Page
   def add_diagnosis(details)
     go_to_tab("Diagnosis")
     fill_in "name-#{details[:index]}", :with => details[:name]
-    page.find("#order-#{details[:index]}").all("button").find {|button| button.text == details[:order] }.click
-    page.find("#certainty-#{details[:index]}").all("button").find {|button| button.text == details[:certainty] }.click
+    order_button(details[:index], details[:order]).click
+    certainty_button(details[:index], details[:certainty]).click
     click_on "accept-button-#{details[:index]}"
+  end
+
+  def certainty_button(index, certainty)
+    page.find("#certainty-#{index}").all("button").find {|button| button.text == certainty }
+  end
+
+  def order_button(index, diagnosis_order)
+    page.find("#order-#{index}").all("button").find { |button| button.text == diagnosis_order }
+  end
+
+  def delete_diagnosis(details)
+
+    go_to_tab("Diagnosis")
+    numberOfDiagnosesBeforeDelete = number_of_diagnoses
+    diagnosis_rows = page.find(".current-diagnosis").all(".diagnosis-row")
+    matching_diagnosis_row = diagnosis_rows.find { |row| row.all('span', :text => details[:name]).count > 0 }
+    matching_diagnosis_row.find(".remove-diagnosis").click
+
+    deleteConfirmMsg = page.accept_alert
+    expect(deleteConfirmMsg).to eq('Are you sure you want to remove this diagnosis?')
+
+    wait_for_overlay_to_be_hidden
+    numberOfDiagnosesAfterDelete = number_of_diagnoses
+
+    expect(numberOfDiagnosesAfterDelete).to eq(numberOfDiagnosesBeforeDelete -1)
+  end
+
+  def number_of_diagnoses
+    page.all(".diagnosis-row").count
   end
 
 end
---
+++ spec/features/existing_patient_diagnosis_delete.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+feature "exisitng patient data verification" do
+  scenario "verify deletion of diagnosis" do
+    patient = {:given_name => "PatientWith DeletableDiagnosis"} # Is very ill. Has been diagnosed with "Similar Diagnosis", "Giardiasis" and "Amebiasis" among others
+    diagnosis = {:index => 0, :name => "UniqueCold#{Time.now.to_i}", :order => 'PRIMARY', :certainty => 'PRESUMED'}
+
+
+    log_in_to_app(:clinical, :location => 'OPD-1') do
+      patient_search_page.view_patient_from_active_tab(patient[:given_name])
+      patient_dashboard_page.start_consultation
+
+      diagnosis_page.add_diagnosis(diagnosis)
+      diagnosis_page.save
+      diagnosis_page.delete_diagnosis(diagnosis)
+    end
+  end
+end
