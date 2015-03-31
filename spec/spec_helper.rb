19436e8d33be66c20f06d3bdfff32f4f0115b04d
Sudhakar <sudhakar82@gmail.com>
2015-03-31 13:23:19 +0530

    Sudhakar, Preethi | #1808 | After every scenario, javascript errors are checked now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,5 +21,14 @@ Headless.new.start if Settings.headless
 Debugger.start
 
 RSpec.configure do |config|
-    config.include AppsAwareness
+  config.include AppsAwareness
+  config.after(:each) do
+    sleep 0.25 #Give time for the browser to set the angular_exception when exception happens during last step
+    exceptions = page.execute_script('return window.angular_exception')
+    if exceptions
+      exceptions_string = exceptions.collect { |e| e['errorMessage']+"\n" + e['stackTrace'].join("\n") }
+      fail "javascript error(s) found!\n#{exceptions_string.join("\n")}"
+      page.execute_script('delete window.angular_exception')
+    end
+  end
 end
