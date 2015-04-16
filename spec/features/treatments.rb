f7a6983fd7fcda9c6c09ed596ddf35df74e28ff4
sravanthi17 <sravanns@thoughtworks.com>
2015-04-16 18:29:47 +0530

    Charles, Sravanthi , Displaying quantity and quantity units in order to fix functional tests

--- spec/features/treatments.rb
+++ spec/features/treatments.rb
@@ -7,15 +7,15 @@ feature "treatments" do
     patient = {:given_name => "Test AddDrugScenario", :current_visit_date => "15 Jan 15"}
     drug1 = {:drug_name => "Albendazole 400mg (Tablet)", :dose => "2", :dose_unit => "Tablet(s)", :frequency => "Twice a day", :sos => false, :start_date => date.strftime("%F"),
              :instructions => "After meals", :duration => "1", :duration_unit => "Day(s)", :drug_route => "Oral", :additional_instructions => "On medication",
-             :quanity => "10", :quantity_units => "Unit(s)"}
+             :quantity => "4", :quantity_units => "Tablet(s)"}
 
     drug2 = {:drug_name => "Albendazole 400mg (Tablet)", :morning_dose => "1.5", :noon_dose => "0", :night_dose => "1", :dose_unit => "Tablet(s)", :sos => true, :start_date => (date + 1).strftime("%F"),
              :instructions => "After meals", :duration => "4", :duration_unit => "Day(s)", :drug_route => "Oral", :additional_instructions => "Take medicine as required",
-             :quanity => "10", :quantity_units => "Unit(s)"}
+             :quantity => "10", :quantity_units => "Tablet(s)"}
 
     drug3 = {:drug_name => "Hepatitis - B 1ml (Injection)", :dose => "1", :dose_unit => "mg", :frequency => "Once a month", :sos => false, :start_date => (date + 20).strftime("%F"),
              :instructions => "In the morning", :duration => "2", :duration_unit => "Month(s)", :drug_route => "Intravenous", :additional_instructions => "Injection to be taken..",
-             :quanity => "10", :quantity_units => "Unit(s)"}
+             :quantity => "2", :quantity_units => "Unit(s)"}
 
     log_in_to_app(:clinical, :location => 'OPD-1') do
       patient_search_page.view_patient_from_all_tab(patient[:given_name])
@@ -53,7 +53,7 @@ feature "treatments" do
 
     ipratropium = {:drug_name => "Ipratropium Bromide Respirator Solution 15ml (Solution)", :morning_dose => "3", :noon_dose => "0", :night_dose => "0", :dose_unit => "ml", :sos => true, :start_date => "15 Jan 15",
                    :instructions => "Immediately", :duration => "3", :duration_unit => "Day(s)", :drug_route => "Intravenous", :additional_instructions => "On medication immediately..",
-                   :quantity => "10", :quantity_units => "Unit(s)"}
+                   :quantity => "9", :quantity_units => "Unit(s)"}
 
     refilled_ipratropium = {:drug_name => "Ipratropium Bromide Respirator Solution 15ml (Solution)", :morning_dose => "3", :noon_dose => "0", :night_dose => "0", :dose_unit => "ml", :sos => true, :start_date => date.strftime("%d %b %y"),
                             :instructions => "Immediately", :duration => "3", :duration_unit => "Day(s)", :drug_route => "Intravenous", :additional_instructions => "On medication immediately..",
