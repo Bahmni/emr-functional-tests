a67cf40d76dd0f13325fca9cd0373be908b2e657
Sudhakar <sudhakar82@gmail.com>
2015-04-17 17:04:15 +0530

    Sudhakar | FunctionalTests | No need to change the config url to local everytime before running in local machine + output in 2 places, console and html results (spec-results folder)

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@
 rvm use 1.9.3
 set -ex
 bundle install
-bundle exec rspec spec/features/*.rb --format html --out results.html
+bundle exec rspec spec/features/*.rb --format documentation --format html --out spec-results/index.html
