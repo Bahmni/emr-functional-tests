d4c52da5226e2df9a97b1c6d71264d7a380a6e7e
Hemanth S.R <hemanthfeb25@gmail.com>
2015-06-12 15:59:23 +0530

    Abhishek, Hemanth | unzipping psql before applying.

--- scripts/setup-database.sh
+++ scripts/setup-database.sh
@@ -4,4 +4,5 @@ DBDUMP_DIR="${BASE_DIR}/dbdump"
 BAHMNI_ENV_SCRIPTS="${BASE_DIR}/../bahmni-environment/scripts"
 
 sh ${BAHMNI_ENV_SCRIPTS}/restore-mysql.sh password ${DBDUMP_DIR}/mysql_backup.sql
+gunzip ${DBDUMP_DIR}/pgsql_backup.sql.gz ${DBDUMP_DIR}/pgsql_backup.sql
 sh ${BAHMNI_ENV_SCRIPTS}/restore-pgsql.sh ${DBDUMP_DIR}/pgsql_backup.sql
