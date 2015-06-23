bbc0dd6c0ab35caa1ab32f4aeb5942191cf201e2
sandeepe <sandeepe@INsandeepe.local>
2015-06-23 14:47:26 +0530

    Sandeep | Removed history of dumpfiles and compressed mysql dump

--- scripts/setup-database.sh
+++ scripts/setup-database.sh
@@ -3,6 +3,7 @@ BASE_DIR="${SCRIPTS_DIR}/.."
 DBDUMP_DIR="${BASE_DIR}/dbdump"
 BAHMNI_ENV_SCRIPTS="${BASE_DIR}/../bahmni-environment/scripts"
 
+gunzip ${DBDUMP_DIR}/mysql_backup.sql.gz ${DBDUMP_DIR}/mysql_backup.sql
 sh ${BAHMNI_ENV_SCRIPTS}/restore-mysql.sh password ${DBDUMP_DIR}/mysql_backup.sql
 gunzip ${DBDUMP_DIR}/pgsql_backup.sql.gz ${DBDUMP_DIR}/pgsql_backup.sql
 sh ${BAHMNI_ENV_SCRIPTS}/restore-pgsql.sh ${DBDUMP_DIR}/pgsql_backup.sql
