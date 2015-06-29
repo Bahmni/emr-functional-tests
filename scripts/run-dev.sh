8064bb7bee94358cf90e7ae1a4f27f30e5740e29
Sudhakar <sudhakar82@gmail.com>
2015-06-29 11:47:17 +0530

    Sudhakar | FunctionalTests | Making the run-dev.sh to run again in the dev boxes (unzipping the db before restoring)

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -8,6 +8,7 @@ DBDUMP_DIR="/bahmni/emr-functional-tests/dbdump"
 BAHMNI_ENV_SCRIPTS="/bahmni/bahmni-environment/scripts"
 
 # Restore only the mysql database
+run_in_vagrant -c "gunzip -c ${DBDUMP_DIR}/mysql_backup.sql.gz > ${DBDUMP_DIR}/mysql_backup.sql"
 run_in_vagrant -c "${BAHMNI_ENV_SCRIPTS}/restore-mysql.sh password ${DBDUMP_DIR}/mysql_backup.sql"
 
 /bin/bash scripts/run.sh
