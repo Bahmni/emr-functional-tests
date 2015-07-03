72518663241a4cde4ca2908d6cead20737b86526
chethanTw <chethandec22@gmail.com>
2015-07-03 15:18:17 +0530

    Chethan | Fixing run-dev.sh

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -5,10 +5,10 @@ source $SCRIPT_DIR/vagrant_functions.sh
 USER=bahmni
 
 DBDUMP_DIR="/bahmni/emr-functional-tests/dbdump"
-BAHMNI_ENV_SCRIPTS="/bahmni/bahmni-environment/scripts"
+BAHMNI_DB_SCRIPTS="/bahmni/emr-functional-tests/scripts"
 
 # Restore only the mysql database
 run_in_vagrant -c "gunzip -c ${DBDUMP_DIR}/mysql_backup.sql.gz > ${DBDUMP_DIR}/mysql_backup.sql"
-run_in_vagrant -c "${SCRIPT_DIR}/restore-mysql.sh password ${DBDUMP_DIR}/mysql_backup.sql"
+run_in_vagrant -c "${BAHMNI_DB_SCRIPTS}/run-mysql-script.sh password ${DBDUMP_DIR}/mysql_backup.sql"
 
 /bin/bash scripts/run.sh
