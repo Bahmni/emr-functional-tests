96dd02138437f6a96e179535680b1a5c71d484ac
Mahesh Kumar <maheshkv@thoughtworks.com>
2015-07-13 12:38:42 +0530

    Mahesh, Sravanthi | Added soak testing specific configuration

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,9 @@
 rvm use 1.9.3
 set -ex
 bundle install
-bundle exec rspec spec/features/*.rb --format documentation --format html --out spec-results/index.html
+if [ "$TEST_ENV" = "soak" ]
+then
+        bundle exec rspec spec/features/{multiple_dashboard_visit_pages.rb,new_patient_visit.rb} --format documentation --format html --out spec-results/index.html
+else
+        bundle exec rspec spec/features/*.rb --format documentation --format html --out spec-results/index.html
+fi
