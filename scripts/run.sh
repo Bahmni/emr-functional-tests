3e7c2592466b5d23e91f3cc3647da84f0e7a4806
Ramprasad <ramyadav@thoughtworks.com>
2015-08-14 19:23:46 +0530

    Ram | Reverting the parallel run

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,9 +5,6 @@ bundle install
 if [ "$TEST_ENV" = "soak" ]
 then
         bundle exec rspec spec/features/{multiple_dashboard_visit_pages.rb,new_patient_visit.rb} --format documentation --format html --out spec-results/index.html
-elif [ "$TEST_ENV" = "ci" ]
-then
-        ruby scripts/parallel_run.rb $PARALLEL_INSTANCES
 else
         bundle exec rspec spec/features/*.rb --format documentation --format html --out spec-results/index.html
 fi
