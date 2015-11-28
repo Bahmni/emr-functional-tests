524b6167123134a40e3c87c4e965fed5eda744b5
Venkata Jaswanth <jaswanth@thoughtworks.com>
2015-11-28 16:22:18 +0530

    Jaswanth | Removing port forwarding in container

--- scripts/setup_for_docker.sh
+++ scripts/setup_for_docker.sh
@@ -2,4 +2,4 @@ rm -f /tmp/emr-functional-tests
 ln -s `pwd`/emr-functional-tests /tmp/emr-functional-tests
 rm -f /tmp/packages
 ln -s `pwd`/rpms /tmp/packages
-(docker run -v /tmp/emr-functional-tests/:/emr-functional-tests -v /tmp/packages:/packages -p 8080:8080 -p 443:443 --name bahmni -d bahmni/bahmni) || true
+(docker run -v /tmp/emr-functional-tests/:/emr-functional-tests -v /tmp/packages:/packages --name bahmni -d bahmni/bahmni) || true
