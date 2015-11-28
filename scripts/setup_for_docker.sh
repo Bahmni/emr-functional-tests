82fba873ee6892c0f2b37c0ad308a7f5e1818895
Venkata Jaswanth <jaswanth@thoughtworks.com>
2015-11-28 16:08:43 +0530

    Jaswanth | Changed approach

--- scripts/setup_for_docker.sh
+++ scripts/setup_for_docker.sh
@@ -1,3 +1,5 @@
-(test -h /tmp/emr-functional-tests) || (ln -s `pwd`/emr-functional-tests /tmp/emr-functional-tests)
-(test -h /tmp/packages) || (ln -s `pwd`/rpms /tmp/packages)
+rm -f /tmp/emr-functional-tests
+ln -s `pwd`/emr-functional-tests /tmp/emr-functional-tests
+rm -f /tmp/packages
+ln -s `pwd`/rpms /tmp/packages
 (docker run -v /tmp/emr-functional-tests/:/emr-functional-tests -v /tmp/packages:/packages -p 8080:8080 -p 443:443 --name bahmni -d bahmni/bahmni) || true
