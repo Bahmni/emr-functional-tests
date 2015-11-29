86893b4c335d77e30c27e7a93fdcfb7cf331bfe8
bharatak <bharatak@thoughtworks.com>
2015-11-29 17:41:45 +0530

    Bharat| Updated docker-run script

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,13 +1,1 @@
-#!/bin/bash --login
-yum remove -y openmrs bahmni-emr bahmni-web
-
-yum localinstall -y /packages/openmrs*.rpm
-yum localinstall -y /packages/bahmni-emr*.rpm
-yum localinstall -y /packages/bahmni-web*.rpm
-
-service openmrs restart
-service httpd restart
-
-sleep 30
-
-bundle exec rspec spec/features/*.rb --format documentation --format html --out spec-results/index.html
+docker exec -it bahmni /emr-functional-tests/scripts/run-inside-docker-container.sh
