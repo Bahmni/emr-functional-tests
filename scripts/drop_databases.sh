1633b87e4ac0b4b3b69cf7c7630c8b72758f65da
hemanths <hemanths@hemanths.local>
2016-06-30 15:37:25 +0530

    Hemanth | Fixing db dump scripts

--- scripts/drop_databases.sh
+++ scripts/drop_databases.sh
@@ -3,5 +3,5 @@ BASE_DIR="${SCRIPTS_DIR}/.."
 DBDUMP_DIR="${BASE_DIR}/dbdump"
 
 sh ${SCRIPTS_DIR}/drop_openmrs_db.sh password
-sh ${SCRIPTS_DIR}/drop_openelis_db.sh
-sh ${SCRIPTS_DIR}/drop_openerp_db.sh
+#sh ${SCRIPTS_DIR}/drop_openelis_db.sh
+#sh ${SCRIPTS_DIR}/drop_openerp_db.sh
--- scripts/setup_databases.sh
+++ scripts/setup_databases.sh
@@ -7,8 +7,8 @@ sh ${SCRIPTS_DIR}/drop_databases.sh
 gunzip -c ${DBDUMP_DIR}/openmrs_backup.sql.gz > ${DBDUMP_DIR}/openmrs_backup.sql
 sh ${SCRIPTS_DIR}/run-mysql-script.sh password ${DBDUMP_DIR}/openmrs_backup.sql
 
-gunzip -c ${DBDUMP_DIR}/openelis_backup.sql.gz > ${DBDUMP_DIR}/openelis_backup.sql
-sh ${SCRIPTS_DIR}/run-pgsql-script.sh ${DBDUMP_DIR}/openelis_backup.sql
-
-gunzip -c ${DBDUMP_DIR}/openerp_backup.sql.gz > ${DBDUMP_DIR}/openerp_backup.sql
-sh ${SCRIPTS_DIR}/run-pgsql-script.sh ${DBDUMP_DIR}/openerp_backup.sql
+#gunzip -c ${DBDUMP_DIR}/openelis_backup.sql.gz > ${DBDUMP_DIR}/openelis_backup.sql
+#sh ${SCRIPTS_DIR}/run-pgsql-script.sh ${DBDUMP_DIR}/openelis_backup.sql
+#
+#gunzip -c ${DBDUMP_DIR}/openerp_backup.sql.gz > ${DBDUMP_DIR}/openerp_backup.sql
+#sh ${SCRIPTS_DIR}/run-pgsql-script.sh ${DBDUMP_DIR}/openerp_backup.sql
