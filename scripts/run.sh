db2271347e243852302f0b6d317087b73b9acf23
abisheka <abishek91@gmail.com>
2016-06-30 10:32:56 +0530

    Abishek | Fixing offline functional tests

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,7 +7,7 @@ then
         bundle exec rspec spec/features/{multiple_dashboard_visit_pages.rb,new_patient_visit.rb} --format documentation --format html --out spec-results/index.html
 elif [ "$TEST_ENV" = "offline-ci" ]
 then
-        bundle exec rspec spec/features/offline/registration.rb --format documentation --format html --out spec-results/index.html
+        bundle exec rspec spec/features/offline/*.rb --format documentation --format html --out spec-results/index.html
 else
         bundle exec rspec spec/features/*.rb --format documentation --format html --out spec-results/index.html
 fi
